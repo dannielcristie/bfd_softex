617db4ce797346901e05439840f5619691646fff
Danniel Cristie <dannielcristie@gmail.com>
2025-08-24 19:00:08 -0300

    exercicio 01

---
+++ atividade01/exercicio01.js
@@ -0,0 +1,8 @@
+// 1. Crie uma variável e atribua um número e, depois mostre na console a tabuada de
+// multiplicação desse número (de 1 a 10).
+
+let numero = 5;
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+};
